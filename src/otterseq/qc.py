983e08b4d7850846dc1198a9a71cc2a39555a0d4
vicentepese <vsempereperis@gmail.com>
2024-08-19 10:02:19 +0200

    feat: filter list ibd

--- src/otterseq/qc.py
+++ src/otterseq/qc.py
@@ -6,6 +6,8 @@ Supported functionalities are:
 import os
 import subprocess
 
+import pandas as pd
+
 import ottersh
 
 
@@ -18,25 +20,58 @@ class OtterQC:
     def __init__(self):  # noqa: D107
         pass
 
-    def ibd(self, filename: str):
+    def ibd(
+        self, filename: str, threshold: float | int = 0.25
+    ) -> pd.DataFrame:
         """Compute Inheritance By Descent (IBD).
+
+        This function leverages PLINK2.0 to compute IBD on a provided filename
+        path (path to file with prefix and no suffix, such as "data/toy"). The output
+        is written in the same directory as the input file ("data/toy.kin0").
 
         Args:
             filename (str): Path to the file with prefix (e.g. `data/toy`,
                 where the "data" folder contains a "toy.map", "toy.bed", and
                 "toy.bim" file).
+            threshold (float | int): Kinship threshold used to filter out
+                individuals.
 
         Raises:
             TypeError: If `filename` is not of type str.
-            FileNotFoundError: If the filename provided does not point to a `.bed`
-                file.
+            TypeError: If `threshold` is not of type int | float.
+            ValueError: If `threshold` is not in between 0 and 1 (included).
         """
         if not isinstance(filename, str):
             raise TypeError(f"filepath not of type str. Got {type(filename)}")
+        if not isinstance(threshold, float | int):
+            raise TypeError(
+                f"threshold not of type int or float. Got {type(threshold)}"
+            )
         if not os.path.isfile(filename + ".bed"):
             raise FileNotFoundError(f"{filename}.bed file not found.")
+        if not 0 <= threshold < 1:
+            raise ValueError(f"threshold not in range (0,1). Got {threshold}")
 
-        command = ["bash", self.IBD_SCRIPT, "--bfile", filename]
+        # Compute IBD
+        command = [
+            "bash",
+            self.IBD_SCRIPT,
+            "--bfile",
+            filename,
+            "--threshold",
+            str(threshold),
+        ]
         subprocess.run(
             command, capture_output=True, text=True, check=False  # noqa: S603
         )
+
+        # Return filtered out patients
+        indv_out = pd.read_csv(
+            filename + ".king.cutoff.out.id",
+            sep=r"\s+",
+            header=0,
+            names=["FID", "IID"],
+            dtype={"FID": str, "IID": str},
+        )
+
+        return indv_out
